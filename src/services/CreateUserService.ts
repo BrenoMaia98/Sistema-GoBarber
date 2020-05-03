9d3b88dd9524d7f4de4001afa79b9c8e3fa10696
BrenoMaia98 <brenomaia98@gmail.com>
2020-05-03 18:28:59 -0300

    Hash na senha ao cadastrar usuário e exclusão do campo senaha na listagem

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -19,10 +20,12 @@ class CreateUserService {
       throw new Error('Email adress alread used.');
     }
 
+    const hasehdPassword = await hash(password, 8);
+
     const user = userRepository.create({
       name,
       email,
-      password,
+      password: hasehdPassword,
     });
 
     await userRepository.save(user);
