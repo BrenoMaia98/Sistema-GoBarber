ffc2cd8eaebb4a98f790d78a8032720499ce0177
BrenoMaia98 <brenomaia98@gmail.com>
2020-05-02 22:51:21 -0300

    Criação tabela appointments e migrations

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,17 +1,16 @@
 import { uuid } from 'uuidv4';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.date = date;
-    this.provider = provider;
-    this.id = uuid();
-  }
 }
 
 export default Appointment;
